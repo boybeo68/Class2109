4d974f91c4c94295b483e9400b24381dc5cb753d
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2021-11-17 19:21:52 +0700

    bonus dom

--- Buoi15/index.js
+++ Buoi15/index.js
@@ -1,8 +1,7 @@
-console.log("hello");
 let tagH1 = document.getElementById("idH1");
 let tagP = document.getElementsByTagName("p");
 let example = document.getElementsByClassName("example");
-console.log(example);
+
 tagH1.style.color = "red";
 for (let index = 0; index < tagP.length; index++) {
   tagP[index].style.color = "blue";
@@ -11,3 +10,26 @@ example[1].style.color = "green";
 
 example[1].innerHTML = " <span style='color: blueviolet'>Data span</span>";
 example[2].innerText = " <span style='color: blueviolet'>Data span</span>";
+// queryselector
+
+let tagH3 = document.querySelector("p");
+let tagH3All = document.querySelectorAll("p");
+let ClassExample = document.querySelectorAll("div .example");
+let idExample = document.querySelector("#idH1");
+console.log(idExample);
+
+let buttonClick = document.querySelector("#clickButton");
+buttonClick.onclick = () => {
+  alert("tét");
+};
+function myFunction() {
+  alert("click from html");
+}
+let data = "tung";
+let listTask = ["new task", "task 2"];
+// localStorage.setItem("meyKey", "Data cua Tung");
+// localStorage.setItem("meyKey2", "Data cua Tung test");
+localStorage.setItem("newTask", JSON.stringify(listTask));
+let getlistTask = localStorage.getItem("newTask");
+// let getData = localStorage.getItem("meyKey2");
+console.log(JSON.parse(getlistTask));
