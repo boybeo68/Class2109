935744f1c692d7c52f4fd0a6a5d9ab1bc83641b2
nguyen thac tung <tung.nguyen.thac@ntq-solution.com.vn>
2021-12-22 08:16:01 +0700

    add dev connect

---
+++ devconect/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+// - Dựng base project
+// - Kiên thức mới. ( Authenication - JWT)
+// - Ôn tập kiến thức cơ bản (State, props, lifecycle, call api, Redux )
+// - Sử dung postman và linh động mock api
+// - Router
+// - Deploy
+
+const App = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
